6a584a53354413d759af1a1f27cac3f57cd6dfed
Scott Reiter <reitersg@dukes.jmu.edu>
2016-11-02 15:31:37 -0400

    All queries are fixed

--- src/main/java/org/cubrc/example/Query.java
+++ src/main/java/org/cubrc/example/Query.java
@@ -5,6 +5,9 @@ import jdk.nashorn.internal.parser.JSONParser;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.ui.Model;
+
+import java.io.IOException;
 
 /**
  * Created by reitersg on 7/21/2016.
@@ -20,12 +23,15 @@ public class Query {
                 "PREFIX time: <http://www.ontologylibrary.mil/CommonCore/Mid/TimeOntology#> \n" +
                 "PREFIX agent: <http://www.ontologylibrary.mil/CommonCore/Mid/AgentOntology#> \n" +
                 "PREFIX info: <http://www.ontologylibrary.mil/CommonCore/Mid/InformationEntityOntology#> \n" +
+                "PREFIX ex: <http://example.org/>\n" +
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "PREFIX event: <http://www.ontologylibrary.mil/CommonCore/Mid/EventOntology#>" +
                 "insert data { \n" +
                 "ex:DayOff rdf:type event:Act .\n" +
-                "ex:DayOff ero:occurs_on ex:DateDay , \n" +
-                "ex:DateMonth , \n" +
-                "ex:DateYear ; \n" +
-                "ex:DayOff info:prescribes ex:DateObjective" +
+                "ex:DayOff ero:occurs_on ex:DateDay . \n" +
+                "ex:DayOff ero:occurs_on ex:DateMonth . \n" +
+                "ex:DayOff ero:occurs_on ex:DateYear . \n" +
+                "ex:DayOff info:prescribes ex:DateObjective . \n" +
                 "ex:DateDay rdf:type time:Day .\n" +
                 "ex:DateDay ero:designator_annotation " + "\"" + date.getDay()+ "\"" + ".\n" +
                 "ex:DateMonth rdf:type time:Month .\n" +
@@ -40,11 +46,13 @@ public class Query {
     public void insertVacation(Vacation vacation){
         RestClient client = new RestClient();
         client.setServerToUpdate();
-        String query = "PREFIX time: <http://www.ontologylibrary.mil/CommonCore/Mid/TimeOntology#>" +
+        String query = "PREFIX ahistory: <http://www.ontologylibrary.mil/CommonCore/Domain/AgentHistoryOntology#>" +
                 "PREFIX ero: <http://www.ontologylibrary.mil/CommonCore/Upper/ExtendedRelationOntology#>" +
+                "PREFIX ex: <http://example.org/>\n" +
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
                 "insert data { \n" +
-                ":Vacation rdf:type time:MultiDayTemporalIdentifier .\n" +
-                ":Vacation ero:occurs_on" + "\"" + vacation.toString() + " - " + vacation.toStringEnd() + "\""
+                "ex:Vacation rdf:type ahistory:ActOfVacationing .\n" +
+                "ex:Vacation ero:occurs_on " + "\"" + vacation.toString() + " - " + vacation.toStringEnd() + "\""
                 + "}";
         client.post("", query);
     }
@@ -56,23 +64,26 @@ public class Query {
                 "PREFIX time: <http://www.ontologylibrary.mil/CommonCore/Mid/TimeOntology#>" +
                 "PREFIX agent: <http://www.ontologylibrary.mil/CommonCore/Mid/AgentOntology#>" +
                 "PREFIX info: <http://www.ontologylibrary.mil/CommonCore/Mid/InformationEntityOntology#>" +
+                "PREFIX ex: <http://example.org/>\n" +
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "PREFIX event: <http://www.ontologylibrary.mil/CommonCore/Mid/EventOntology#> \n" +
                 "insert data { \n" +
                 "ex:TimeOff rdf:type event:Act .\n" +
-                "ex:TimeOff ero:occurs_on ex:TimeDay , \n" +
-                "ex:TimeMonth , \n" +
-                "ex:TimeYear , \n" +
-                "ex:TimeTime ;" +
-                "ex:TimeOff info:prescribes ex:TimeObjective" +
-                "ex:TimeDay rdf:type time:Day .\n" +
-                "ex:TimeDay ero:designator_annotation " + "\"" + time.getDay()+ "\"" + ".\n" +
+                "ex:TimeOff ero:occurs_on ex:TimeDay . \n" +
+                "ex:TimeOff ero:occurs_on ex:TimeMonth . \n" +
+                "ex:TimeOff ero:occurs_on ex:TimeYear . \n" +
+                "ex:TimeOff ero:occurs_on ex:TimeTime . \n" +
+                "ex:TimeOff info:prescribes ex:TimeObjective . \n" +
+                "ex:TimeDay rdf:type  time:Day .\n" +
+                "ex:TimeDay ero:designator_annotation " + "\"" + time.getDay()+ "\"" + ". \n" +
                 "ex:TimeMonth rdf:type time:Month .\n" +
-                "ex:TimeMonth ero:designator_annotation " + "\"" + time.getMonth()+ "\"" + ".\n" +
-                "ex:TimeYear rdf:type time:Year" +
-                "ex:TimeYear ero:designator_annotation " + "\"" + time.getYear()+ "\"" + ".\n" +
+                "ex:TimeMonth ero:designator_annotation " + "\"" + time.getMonth()+ "\"" + ". \n" +
+                "ex:TimeYear rdf:type time:Year ." +
+                "ex:TimeYear ero:designator_annotation " + "\"" + time.getYear()+ "\"" + ". \n" +
                 "ex:TimeTime rdf:type time:MultiHourTemporalIdentifier . \n" +
-                "ex:TimeTime ero:designator_annotation" + "\"" + time.getTime() + "\"" + ".\n" +
+                "ex:TimeTime ero:designator_annotation " + "\"" + time.getTime() + "\"" + ". \n" +
                 "ex:TimeObjective rdf:type agent:Objective . \n" +
-                "ex:TimeObjective info:prescribes " + "\"" + time.getPurpose() + "\"" + ".\n" +
+                "ex:TimeObjective info:prescribes " + "\"" + time.getPurpose() + "\"" + ". \n" +
                 " } ";
         client.post("", query);
     }
@@ -83,15 +94,18 @@ public class Query {
                 + "PREFIX time: <http://www.ontologylibrary.mil/CommonCore/Mid/TimeOntology#>" +
                 "PREFIX agent: <http://www.ontologylibrary.mil/CommonCore/Mid/AgentOntology#>" +
                 "PREFIX info: <http://www.ontologylibrary.mil/CommonCore/Mid/InformationEntityOntology#>" +
+                "PREFIX ex: <http://example.org/>\n" +
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "PREFIX event: <http://www.ontologylibrary.mil/CommonCore/Mid/EventOntology#> \n" +
                 "select ?day ?month ?year ?purpose {\n" +
                 "ex:DayOff rdf:type event:Act .\n" +
-                "ex:DayOff ero:occurs_on ex:DateDay , \n" +
-                "ex:DateMonth , \n" +
-                "ex:DateYear ; \n" +
-                "ex:DayOff info:prescribes ex:DateObjective" +
+                "ex:DayOff ero:occurs_on ex:DateDay . \n" +
+                "ex:DayOff ero:occurs_on ex:DateMonth . \n" +
+                "ex:DayOff ero:occurs_on ex:DateYear . \n" +
+                "ex:DayOff info:prescribes ex:DateObjective ." +
                 "ex:DateDay rdf:type time:Day .\n" +
                 "ex:DateDay ero:designator_annotation ?day . \n" +
-                "ex:DayMonth rdf:type time:Month .\n" +
+                "ex:DateMonth rdf:type time:Month .\n" +
                 "ex:DateMonth ero:designator_annotation ?month . \n" +
                 "ex:DateYear rdf:type time:Year .\n" +
                 "ex:DateYear ero:designator_annotation ?year . \n" +
@@ -105,8 +119,11 @@ public class Query {
         RestClient client = new RestClient();
         client.setServerToQuery();
         String query = "PREFIX ero: <http://www.ontologylibrary.mil/CommonCore/Upper/ExtendedRelationOntology#>" +
-                "PREFIX time: <http://www.ontologylibrary.mil/CommonCore/Mid/TimeOntology#>" +
-                "select ?vacation where {time:MultiDayTemporalIdentifier ero:occurs_on ?vacation } ";
+                "PREFIX ahistory: <http://www.ontologylibrary.mil/CommonCore/Domain/AgentHistoryOntology#>" +
+                "PREFIX ex: <http://example.org/>\n" +
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "select ?vacation where {ex:Vacation rdf:type ahistory:ActOfVacationing .\n" +
+                "ex:Vacation ero:occurs_on ?vacation } ";
         String response = client.post("", query);
         return response;
     }
@@ -118,19 +135,22 @@ public class Query {
                 + "PREFIX time: <http://www.ontologylibrary.mil/CommonCore/Mid/TimeOntology#>" +
                 "PREFIX agent: <http://www.ontologylibrary.mil/CommonCore/Mid/AgentOntology#>" +
                 "PREFIX info: <http://www.ontologylibrary.mil/CommonCore/Mid/InformationEntityOntology#>" +
-                "select ?day ?month ?year ?purpose { \n" +
+                "PREFIX ex: <http://example.org/>\n" +
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "PREFIX event: <http://www.ontologylibrary.mil/CommonCore/Mid/EventOntology#> \n" +
+                "select ?day ?month ?year ?time ?purpose { \n" +
                 "ex:TimeOff rdf:type event:Act .\n" +
-                "ex:TimeOff ero:occurs_on ex:TimeDay , \n" +
-                "ex:TimeMonth , \n" +
-                "ex:TimeYear , \n" +
-                "ex:TimeTime ;" +
-                "ex:TimeOff info:prescribes ex:TimeObjective" +
+                "ex:TimeOff ero:occurs_on ex:TimeDay . \n" +
+                "ex:TimeOff ero:occurs_on ex:TimeMonth . \n" +
+                "ex:TimeOff ero:occurs_on ex:TimeYear . \n" +
+                "ex:TimeOff ero:occurs_on ex:TimeTime . \n" +
+                "ex:TimeOff info:prescribes ex:TimeObjective . \n" +
                 "ex:TimeDay rdf:type time:Day .\n" +
                 "ex:TimeDay ero:designator_annotation ?day . \n" +
                 "ex:TimeMonth rdf:type time:Month .\n" +
                 "ex:TimeMonth ero:designator_annotation ?month . \n" +
-                "ex:DateYear rdf:type time:Year .\n" +
-                "ex:DateYear ero:designator_annotation ?year . \n" +
+                "ex:TimeYear rdf:type time:Year .\n" +
+                "ex:TimeYear ero:designator_annotation ?year . \n" +
                 "ex:TimeTime rdf:type time:MultiHourTemporalIdentifier . \n" +
                 "ex:TimeTime ero:designator_annotation ?time" + ".\n" +
                 "ex:TimeObjective rdf:type agent:Objective . \n" +
@@ -146,11 +166,14 @@ public class Query {
                 "PREFIX time: <http://www.ontologylibrary.mil/CommonCore/Mid/TimeOntology#>" +
                 "PREFIX agent: <http://www.ontologylibrary.mil/CommonCore/Mid/AgentOntology#>" +
                 "PREFIX info: <http://www.ontologylibrary.mil/CommonCore/Mid/InformationEntityOntology#>" +
+                "PREFIX ex: <http://example.org/>\n" +
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "PREFIX event: <http://www.ontologylibrary.mil/CommonCore/Mid/EventOntology#> \n" +
                 "delete where {\n" +
                 "ex:DayOff rdf:type event:Act .\n" +
-                "ex:DayOff ero:occurs_on ex:DateDay , \n" +
-                "ex:DateMonth , \n" +
-                "ex:DateYear ; \n" +
+                "ex:DayOff ero:occurs_on ex:DateDay . \n" +
+                "ex:DayOff ero:occurs_on ex:DateMonth . \n" +
+                "ex:DayOff ero:occurs_on ex:DateYear . \n" +
                 "ex:DayOff info:prescribes ex:DateObjective" +
                 "ex:DateDay rdf:type time:Day .\n" +
                 "ex:DateDay ero:designator_annotation " + "\"" + date.getDay()+ "\"" + ".\n" +
@@ -168,8 +191,11 @@ public class Query {
         RestClient client = new RestClient();
         client.setServerToQuery();
         String query = "PREFIX ero: <http://www.ontologylibrary.mil/CommonCore/Upper/ExtendedRelationOntology#>" +
-                "PREFIX time: <http://www.ontologylibrary.mil/CommonCore/Mid/TimeOntology#>" +
-                "delete where {time:MultiDayTemporalIdentifier ero:occurs_on"+ "\"" + vacation.toString() + " - " + vacation.toStringEnd() + "\"" + " } ";
+                "PREFIX ahistory: <http://www.ontologylibrary.mil/CommonCore/Domain/AgentHistoryOntology#>" +
+                "PREFIX ex: <http://example.org/>\n" +
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "delete where {ex:Vacation rdf:type ahistory:ActOfVacationing . \n" +
+                "ex:Vacation ero:occurs_on"+ "\"" + vacation.toString() + " - " + vacation.toStringEnd() + "\"" + " } ";
         String response = client.post("", query);
     }
 
@@ -181,18 +207,21 @@ public class Query {
                 "PREFIX time: <http://www.ontologylibrary.mil/CommonCore/Mid/TimeOntology#>" +
                 "PREFIX agent: <http://www.ontologylibrary.mil/CommonCore/Mid/AgentOntology#>" +
                 "PREFIX info: <http://www.ontologylibrary.mil/CommonCore/Mid/InformationEntityOntology#>" +
+                "PREFIX ex: <http://example.org/>\n" +
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "PREFIX event: <http://www.ontologylibrary.mil/CommonCore/Mid/EventOntology#> \n" +
                 "delete where { \n" +
                 "ex:TimeOff rdf:type event:Act .\n" +
-                "ex:TimeOff ero:occurs_on ex:TimeDay , \n" +
-                "ex:TimeMonth , \n" +
-                "ex:TimeYear , \n" +
-                "ex:TimeTime ;" +
-                "ex:TimeOff info:prescribes ex:TimeObjective" +
+                "ex:TimeOff ero:occurs_on ex:TimeDay . \n" +
+                "ex:TimeOff ero:occurs_on ex:TimeMonth . \n" +
+                "ex:TimeOff ero:occurs_on ex:TimeYear . \n" +
+                "ex:TimeOff ero:occurs_on ex:TimeTime . \n" +
+                "ex:TimeOff info:prescribes ex:TimeObjective . \n" +
                 "ex:TimeDay rdf:type time:Day .\n" +
                 "ex:TimeDay ero:designator_annotation " + "\"" + time.getDay()+ "\"" + ".\n" +
                 "ex:TimeMonth rdf:type time:Month .\n" +
                 "ex:TimeMonth ero:designator_annotation " + "\"" + time.getMonth()+ "\"" + ".\n" +
-                "ex:TimeYear rdf:type time:Year" +
+                "ex:TimeYear rdf:type time:Year ." +
                 "ex:TimeYear ero:designator_annotation " + "\"" + time.getYear()+ "\"" + ".\n" +
                 "ex:TimeTime rdf:type time:MultiHourTemporalIdentifier . \n" +
                 "ex:TimeTime ero:designator_annotation" + "\"" + time.getTime() + "\"" + ".\n" +
@@ -200,6 +229,18 @@ public class Query {
                 "ex:TimeObjective info:prescribes " + "\"" + time.getPurpose() + "\"" + ".\n" +
                 " } ";
         String response = client.post("", query);
+    }
+
+    public void allData(Model model) throws IOException {
+        String dates = this.selectDate();
+        String times = this.selectTime();
+        String vacations = this.selectVacation();
+        StringBuilder sb = DaysOffParser.parseDate(dates);
+        StringBuilder time = DaysOffParser.parseTime(times);
+        StringBuilder vacation = DaysOffParser.parseVacation(vacations);
+        model.addAttribute("dates", sb.toString());
+        model.addAttribute("times", time.toString());
+        model.addAttribute("vacations", vacation.toString());
     }
 
 }
